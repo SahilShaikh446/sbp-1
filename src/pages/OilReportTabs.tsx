bab0cf45e2c65c4e52ad824876944ff3a512ffe7
sohailxdev <100mohdsohail@gmail.com>
2025-08-10 23:11:13 +0530

    for client login create oil report page should not visible that part is fix now deploying to production

--- src/pages/OilReportTabs.tsx
+++ src/pages/OilReportTabs.tsx
@@ -1,21 +1,30 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import OilReportCreate from "../features/oilReport/OilReportCreate";
 import OilReportList from "@/features/oilReport/OilReportList";
+import { useAppSelector } from "@/app/hooks";
+import { selectRole } from "@/features/authSlice/authSlice";
 
 function OilReportTabs() {
+  const role = useAppSelector(selectRole);
   return (
-    <Tabs defaultValue="view Report" className="w-full">
-      <TabsList>
-        <TabsTrigger value="view Report">View Report</TabsTrigger>
-        <TabsTrigger value="create Report">Create Report</TabsTrigger>
-      </TabsList>
-      <TabsContent value="create Report">
-        <OilReportCreate />
-      </TabsContent>
-      <TabsContent value="view Report">
+    <>
+      {role == "Master Admin" ? (
+        <Tabs defaultValue="view Report" className="w-full">
+          <TabsList>
+            <TabsTrigger value="view Report">View Report</TabsTrigger>
+            <TabsTrigger value="create Report">Create Report</TabsTrigger>
+          </TabsList>
+          <TabsContent value="create Report">
+            <OilReportCreate />
+          </TabsContent>
+          <TabsContent value="view Report">
+            <OilReportList />
+          </TabsContent>
+        </Tabs>
+      ) : (
         <OilReportList />
-      </TabsContent>
-    </Tabs>
+      )}
+    </>
   );
 }
 
