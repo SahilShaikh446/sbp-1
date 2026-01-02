4dc784b8b0b5cffb3e5018c5e7f15ca29a85343a
sohailxdev <100mohdsohail@gmail.com>
2026-01-02 10:00:20 +0530

    base url change to deployment url

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,7 +1,7 @@
 // export const BASE_URL = "https://reportportal-1gl0.onrender.com/";
 // export const BASE_URL = "http://192.168.0.102:8080";
-export const BASE_URL = "https://report.okagencies.in/report/";
-// export const BASE_URL = "/report/";
+// export const BASE_URL = "https://report.okagencies.in/report/";
+export const BASE_URL = "/report/";
 
 // Master Admin
 // "username":"master",
